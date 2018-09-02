163902dce296e853f7001f101eb8963ce7037dc7
mikan3rd <mikan.no.kikoushi@gmail.com>
2018-09-02 15:14:50 +0900

    test: botの動作確認

--- bot.py
+++ bot.py
@@ -6,17 +6,17 @@ from app.tasks import instagram
 sched = BlockingScheduler(timezone="Asia/Tokyo")
 
 
-@sched.scheduled_job('interval', minutes=20)
-def timed_job():
-    print('This job runs every 20 minutes.')
-
-
 @sched.scheduled_job('interval', seconds=30)
 def timed_job_seconds():
     print('This job runs every 30 seconds.')
 
 
-@sched.scheduled_job('cron', hour=6)
+@sched.scheduled_job('interval', minutes=1)
+def timed_job_minitutes():
+    print('This job runs every 1 minutes.')
+
+
+@sched.scheduled_job('cron', hour='8,20')
 def instagram_job():
     print('Instagram START!!')
     instagram.update_hashtag()
