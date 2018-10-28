9be2b9316ac37c15881742725e832e31a6f8e800
mikan3rd <mikan.no.kikoushi@gmail.com>
2018-10-28 17:37:23 +0900

    fix: 不要な記述を削除

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
-from datetime import datetime
-from time import sleep
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from app.tasks import instagram, qiita, tiktok
+from app.tasks import instagram, tiktok
 
 
 sched = BlockingScheduler(timezone="Asia/Tokyo")
@@ -23,22 +21,6 @@ def tiktok_job():
     print('TikTok START!!')
     tiktok.get_feed()
     tiktok.update_user_detail()
-
-
-# @sched.scheduled_job('cron', hour=9)
-# def qiita_job():
-#     print('Qiita START!!')
-#     body = tiktok.create_markdown()
-#     now = datetime.now().strftime("%Y/%m/%d")
-#     title = '【自動更新】TikTok人気ユーザーランキング【%s】' % (now)
-#     tags = [{'name': 'Python'}, {'name': 'ランキング'}, {'name': '自動生成'}, {'name': 'tiktok'}]
-
-#     qiita.patch_item(
-#         item_id="b34002f5a9ecaaaacf09",
-#         title=title,
-#         body=body,
-#         tags=tags,
-#     )
 
 
 print("Scheduler START!!")
