3342f8aacc284eb0c6dbbabb7eb58385ca29c37c
mikan3rd <mikan.no.kikoushi@gmail.com>
2018-10-30 20:50:26 +0900

    fix: tiktok定期処理の時間修正

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ def instagram_job():
     print('Instagram FINISH!!')
 
 
-@sched.scheduled_job('interval', minutes=20)
+@sched.scheduled_job('cron', hour='*')
 def tiktok_job():
     print('TikTok START!!')
     tiktok.get_feed()
