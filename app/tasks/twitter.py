c3919d9e124c501fe5c7031d071a9cf6407f4848
mikan3rd <mikan.no.kikoushi@gmail.com>
2018-11-20 14:06:47 +0900

    fix: ツイート長さ調整

--- app/tasks/twitter.py
+++ app/tasks/twitter.py
@@ -267,7 +267,7 @@ def search_and_retweet(account):
         status = '%s\n%s の人気ツイート\n\n@%s %s\n%s' \
             % (now, api.hashtag, user['screen_name'], user['name'], user['description'])
 
-        length = 200
+        length = 190
         status = status[:length] + ('...' if status[length:] else '')
         status = re.sub('(http|#|@)\S*\.\.\.', '...', status)
         print(len(status))
